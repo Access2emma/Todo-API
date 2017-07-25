205631d5ddd7bca1c581dde356267dd6d48519a5
Emmanuel Adeniyi <access2emma@gmail.com>
2017-07-25 02:45:13 +0100

    Updated the process env for horuku to connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp', {useMongoClient: true});
 
 module.exports = {mongoose}
